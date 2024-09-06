613a31c264d75ab3ca6d0d771a1bf399cb13f276
john silvestre <johnkerlin52@gmail.com>
2024-09-06 11:11:43 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Electiva2_repo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Tu nombre es {name}");
+        }
+    }
+}
